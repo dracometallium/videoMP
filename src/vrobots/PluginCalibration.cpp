1750e9e201d4c2b22d9f7eb976edfc7b4760797c
Draco Metallium <draco.kun@gmail.com>
2015-12-21 18:27:21 +0100

    Avoid critical zone in PluginCalibration.

--- src/vrobots/PluginCalibration.cpp
+++ src/vrobots/PluginCalibration.cpp
@@ -17,14 +17,23 @@ int PluginCalibration::process(Item * item)
 	Frame *frame;
 	IplImage *t;
 	frame = (Frame *) item;
+	if (i == 0)
 #pragma omp critical
-	if (i == 0) {
-		intrinsic = (CvMat *) cvLoad("src/vrobots/Intrinsics.xml");
-		distortion = (CvMat *) cvLoad("src/vrobots/Distortion.xml");
-		mapx = cvCreateImage(cvGetSize(frame->frame), IPL_DEPTH_32F, 1);
-		mapy = cvCreateImage(cvGetSize(frame->frame), IPL_DEPTH_32F, 1);
-		cvInitUndistortMap(intrinsic, distortion, mapx, mapy);
-		i++;
+	{
+		if (i == 0) {
+			intrinsic =
+			    (CvMat *) cvLoad("src/vrobots/Intrinsics.xml");
+			distortion =
+			    (CvMat *) cvLoad("src/vrobots/Distortion.xml");
+			mapx =
+			    cvCreateImage(cvGetSize(frame->frame),
+					  IPL_DEPTH_32F, 1);
+			mapy =
+			    cvCreateImage(cvGetSize(frame->frame),
+					  IPL_DEPTH_32F, 1);
+			cvInitUndistortMap(intrinsic, distortion, mapx, mapy);
+			i++;
+		}
 	}
 	t = cvCloneImage(frame->frame);
 	cvRemap(t, frame->frame, mapx, mapy);	// Undistort image
